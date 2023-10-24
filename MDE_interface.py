85e514cf98aea044a31326c1f0c1479d9c89e2c2
Logan Um <lum@vt.edu>
2023-10-24 18:48:06 -0400

    Added frequency of wave

--- MDE_interface.py
+++ MDE_interface.py
@@ -20,3 +20,28 @@ plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.grid(True)
 plt.show()
+############################################
+# Load the .wav file
+sample_rate, audio_data = wavfile.read("testwav.wav")
+
+# Check the properties of the audio file
+print("Sample Rate:", sample_rate)
+print("Audio Data Shape:", audio_data.shape)
+
+# Calculate the Fast Fourier Transform (FFT) of the audio data
+fft_result = np.fft.fft(audio_data)
+fft_result = np.abs(fft_result)  # Take the absolute value to get magnitude
+
+# Calculate the frequency values corresponding to the FFT result
+num_samples = len(fft_result)
+frequencies = np.fft.fftfreq(num_samples, 1.0 / sample_rate)
+
+# Plot the frequency content
+plt.figure(figsize=(10, 6))
+plt.plot(frequencies, fft_result)
+plt.title("Frequency Content of Audio File")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.grid(True)
+plt.xlim(0, sample_rate / 2)  # Only display the positive frequencies (up to Nyquist)
+plt.show()
