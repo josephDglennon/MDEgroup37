1912dcb44b506dce31a102e74d8d81de4ce7944e
Jackson Burns <jc3urns@vt.edu>
2023-10-30 13:36:00 -0400

    outlined python interface

---
+++ dmg_assessment/database_manager.py
@@ -0,0 +1,28 @@
+'''
+Manages the database or other storage mechanism we decide to implement
+'''
+class DatabaseManager():
+
+    '''
+    Create and add a new entry to the database
+    '''
+    def save_new_entry(data):
+        return
+    
+    '''
+    Remove an entry from database
+    '''
+    def delete_entry(self, identifier):
+        return
+
+    '''
+    Return a list of identifiers representing the entries thusfar recorded
+    '''
+    def get_entries(self):
+        return  
+    
+    '''
+    Return the data associated with an entry
+    '''
+    def get_entry(self, identifier):
+        return
---
+++ dmg_assessment/dmg_assessment.py
@@ -0,0 +1,13 @@
+
+from device_controller import DeviceController
+
+def main():
+    controller = DeviceController()
+
+# pyqt gui
+class GUI():
+    def __init__(self):
+        True
+
+if __name__ == '__main__':
+    main()
