e50c14a72ff9cde45a1160441c274076757bed07
Jackson Burns <jc3urns@vt.edu>
2023-11-16 20:11:34 -0500

    basic recording functionality

--- src/hardware_input.py
+++ src/hardware_input.py
@@ -3,87 +3,90 @@ import sounddevice
 import soundfile
 import os
 import sys
-import queue
+import numpy
 
-from threading import Thread
 from numpy import ndarray
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 # todo: eventually implement a device manager to pick the desired microphone
 
 @dataclass
 class DmgData():
-    '''
-    Data carrier object inter-module device operations
-    '''
-    audio_data: ndarray
-    trigger_data: ndarray
-    sample_rate: int
+    """Data carrier object inter-module device operations"""
+    audio_data: ndarray = field(init=False)
+    trigger_data: ndarray = field(init=False)
+    sample_rate: int = field(init=False)
 
 class HardwareInput():
-    '''
-    Manages the microphone and any inputs relevant to the recording of a data sample
-    '''
+    """Manages the microphone and any inputs relevant to the recording of a data sample"""
 
     def __init__(self):
-        '''
-        Set up recorder
-        '''
-        
-        self.audio_input_device_ID = 1
-        self.device_info = sounddevice.query_devices(self.audio_input_device_ID)
-        self.sample_rate = int(self.device_info['default_samplerate'])
-        self.channels = 1 #int(self.device_info[''])
+        """Set up recorder"""
 
-        self.is_recording = False
-        self.audioQueue = queue.Queue()
+        self._is_recording = False
+    
+        self._audio_blocks = []
+
+        def callback(indata, frames, time, status):
+            """callback for consumption of audio data from stream"""
+
+            nonlocal self
+
+            if status:
+                print(status, file=sys.stderr)
+
+            self._audio_blocks.append(indata.copy())
+
+        self._audio_input_device_ID = 1
+        self._device_info = sounddevice.query_devices(self._audio_input_device_ID)
+        self._sample_rate = int(self._device_info['default_samplerate'])
+        self._channels = int(self._device_info['max_input_channels'])
+
+        self._audio_stream = sounddevice.InputStream(samplerate=self._sample_rate, device=self._audio_input_device_ID,
+                                    channels=self._channels, callback=callback)
         
 
     
     def start_recording(self):
-        '''
-        Begin recording data from inputs to ndArray queue
-        '''
+        """Begin recording data from inputs to ndArray queue"""
 
-        self.audioQueue = queue.Queue()
-        self.is_recording = True
+        self._audio_blocks = []
+        self._is_recording = True
 
-        def stream():
-
-            def callback(indata, frames, time, status):
-                nonlocal self
-                print('callback')
-                if status:
-                    print(status, file=sys.stderr)
-                self.audioQueue.put(indata.copy())
-            
-            sounddevice.InputStream(samplerate=self.sample_rate, device=self.audio_input_device_ID,
-                                     channels=self.channels, callback=callback)
+        self._audio_stream.start()
 
 
-        recording_thread = Thread(target=stream)
-        recording_thread.start()
     
     def stop_recording(self):
-        '''
-        Stop recording sample
-        '''
-        return
+        """Stop recording sample"""
+
+        self._is_recording = False
+
+        self._audio_stream.stop()
     
     
     def get_data(self):
-        '''
-        Return a SampleData object containing audio array, trigger array, and samplerate
-        '''
-        return
+        """Return a SampleData object containing audio array, trigger array, and samplerate"""
+
+        if self._is_recording: raise Exception
+
+        output_data = DmgData()
+        output_data.sample_rate = self._sample_rate
+        output_data.audio_data = numpy.concatenate(self._audio_blocks)
+
+        return output_data
     
 def main():
 
     hardware = HardwareInput()
     hardware.start_recording()
+    time.sleep(3)
+    hardware.stop_recording()
 
-    #while True:
-        #print('blah')
+    data = hardware.get_data()
+    sounddevice.play(data.audio_data, data.sample_rate)
+    time.sleep(4)
+    sounddevice.stop()
     
     '''
     #sounddevice.default.channels = 1, 5
